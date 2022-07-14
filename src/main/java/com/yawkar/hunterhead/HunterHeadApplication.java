192fdb555d1e4484b1556901749ba3e016387e5f
Yawkar <yawkarpov@gmail.com>
2022-07-14 21:21:04 +0300

    Add base project

---
+++ src/main/java/com/yawkar/hunterhead/HunterHeadApplication.java
@@ -0,0 +1,13 @@
+package com.yawkar.hunterhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunterHeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunterHeadApplication.class, args);
+    }
+
+}
